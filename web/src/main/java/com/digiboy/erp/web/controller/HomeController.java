575b8ad7a6a333c13bff1a29953bcad3866b3ebb
Hossein Mohammadi <hossein.mohammadi@outlook.com>
2021-03-06 22:58:04 +0330

    2

--- web/src/main/java/com/digiboy/erp/web/controller/HomeController.java
+++ web/src/main/java/com/digiboy/erp/web/controller/HomeController.java
@@ -1,2 +1,17 @@
-package com.digiboy.erp.web.controller;public class HomeController {
+package com.digiboy.erp.web.controller;
+
+import org.slf4j.Logger;
+
+import javax.inject.Inject;
+import javax.inject.Named;
+
+@Named("home")
+public class HomeController {
+    @Inject
+    private Logger logger;
+
+    public String index() {
+        logger.info("Forward to index");
+        return "index";
+    }
 }
