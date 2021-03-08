3172ec58c37a955d6eb62642268d7ef74267be62
Hossein Mohammadi <hossein.mohammadi@outlook.com>
2021-03-08 07:29:30 +0330

    ERP

--- web/src/main/java/com/digiboy/erp/web/controller/HomeController.java
+++ web/src/main/java/com/digiboy/erp/web/controller/HomeController.java
@@ -1,9 +1,13 @@
 package com.digiboy.erp.web.controller;
 
+import org.apache.shiro.util.ByteSource;
+import org.apache.shiro.util.SimpleByteSource;
 import org.slf4j.Logger;
 
 import javax.inject.Inject;
 import javax.inject.Named;
+import java.io.IOException;
+import java.io.InputStream;
 
 @Named("home")
 public class HomeController {
@@ -11,6 +15,9 @@ public class HomeController {
     private Logger logger;
 
     public String index() {
+        ByteSource byteSource = new SimpleByteSource(getClass().getClassLoader().getResourceAsStream("logback.xml"));
+        System.out.println(new String(byteSource.getBytes()));
+
         logger.info("Forward to index");
         return "index";
     }
